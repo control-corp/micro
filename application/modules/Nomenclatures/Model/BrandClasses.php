b3237b848639e2b739761e038b1caed793088422
control-corp <control.corp@abv.bg>
2016-02-24 15:06:15 +0200

    Modified   application/modules/Nomenclatures/Model/BrandClasses.php Modified   application/modules/Nomenclatures/Model/Continents.php Missing    application/modules/Nomenclatures/Model/Factory/BrandClassesFactory.php Missing    application/modules/Nomenclatures/Model/Factory/ContinentsFactory.php

--- application/modules/Nomenclatures/Model/BrandClasses.php
+++ application/modules/Nomenclatures/Model/BrandClasses.php
@@ -6,4 +6,7 @@ use Micro\Model\DatabaseAbstract;
 
 class BrandClasses extends DatabaseAbstract
 {
+    protected $table = Table\BrandClasses::class;
+
+    protected $entity = Entity\BrandClass::class;
 }
--- application/modules/Nomenclatures/Model/Continents.php
+++ application/modules/Nomenclatures/Model/Continents.php
@@ -6,4 +6,7 @@ use Micro\Model\DatabaseAbstract;
 
 class Continents extends DatabaseAbstract
 {
+    protected $table = Table\Continents::class;
+
+    protected $entity = Entity\Continent::class;
 }
