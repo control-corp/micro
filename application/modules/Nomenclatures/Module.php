754f0d2730b0a0e88f0edc4b0ddba02aadf79c2b
control-corp <control.corp@abv.bg>
2016-02-24 15:06:52 +0200

    Modified   application/modules/Nomenclatures/Module.php

--- application/modules/Nomenclatures/Module.php
+++ application/modules/Nomenclatures/Module.php
@@ -6,15 +6,4 @@ use Micro\Application\Module as BaseModule;
 
 class Module extends BaseModule
 {
-    public function getConfig()
-    {
-        return [
-            'dependencies' => [
-                'services' => [
-                    Model\BrandClasses::class => Model\Factory\BrandClassesFactory::class,
-                    Model\Continents::class => Model\Factory\ContinentsFactory::class,
-                ],
-            ],
-        ];
-    }
 }
